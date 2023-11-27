17a181eb651cb71c52509c59b263b4c31b337765
ShashankShekhar9839 <111214162+ShashankShekhar9839@users.noreply.github.com>
2023-11-27 23:56:16 +0530

    basic changes

---
+++ library-project/src/stories/Card/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './card.module.scss';
+
+function Card () {
+    return (
+         <div className='card-wrapper' >Card component</div>
+    )
+}
+
+
+export default Card;
